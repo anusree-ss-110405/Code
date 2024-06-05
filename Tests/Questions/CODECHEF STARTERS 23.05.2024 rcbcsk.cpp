be64e1642fb2fb5e08eb0bb27d02086d19cfe175
anusree-ss-110405 <sreeanu110405@gmail.com>
2024-06-05 17:19:18 +0530

    repeated commit

---
+++ Tests/Questions/CODECHEF STARTERS 23.05.2024 rcbcsk.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int rcb,csk;
+    cin>>rcb>>csk;
+    if(rcb-csk >= 18){
+        cout<<"RCB";
+    }
+    else{
+        cout<<"CSK";
+    }
+}
