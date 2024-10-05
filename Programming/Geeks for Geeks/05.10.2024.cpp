6b1ae9602dcf1f6526140f1be68241fe0baea2da
anusree-ss-110405 <sreeanu110405@gmail.com>
2024-10-05 22:21:07 +0530

    05.10.2024

---
+++ Programming/Geeks for Geeks/05.10.2024.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    long long findSmallest(vector<int> &arr) {
+        // Your code goes here.
+         int ans=1;
+        for(int x:arr){
+            if(x>ans) return ans;
+            ans += x;
+        }
+        return ans;
+    }
+};
