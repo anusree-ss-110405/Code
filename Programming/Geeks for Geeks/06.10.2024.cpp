6e6b485fadce3ff029d010278168f071a881cdba
anusree-ss-110405 <sreeanu110405@gmail.com>
2024-10-06 21:37:16 +0530

    06.10.2024

---
+++ Programming/Geeks for Geeks/06.10.2024.cpp
@@ -0,0 +1,31 @@
+void solve(int i,int j, vector<vector<int>>&visited, vector<vector<char>>&grid, int n, int m){
+        visited[i][j]=1;
+        int dx[]={-1,-1,-1,0,0,1,1,1};
+        int dy[]={-1,0,1,-1,1,-1,0,1};
+        for(int k=0;k<8;k++){
+            int nx=i+dx[k];
+            int ny=j+dy[k];
+            if(nx>=0 && ny>=0 && nx<n && ny<m && grid[nx][ny]=='1' && visited[nx][ny]==0){
+                solve(nx,ny,visited,grid,n,m);
+            }
+        }
+    }
+    // Function to find the number of islands.
+    int numIslands(vector<vector<char>>& grid) {
+        // Code here
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>visited(n,vector<int>(m,0));
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1' && visited[i][j]==0){
+                    count++;
+                    solve(i,j,visited,grid,n,m);
+                }
+            }
+        }
+        return count;
+    }
+};
+
